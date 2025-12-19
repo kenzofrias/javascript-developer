02e659221aa73f3dcc342a44cd0545edef1d4c1f
Kenzo Friás <kenzofrias16@gmail.com>
2025-12-19 13:05:44 -0300

    modelo padrão para cada pokemon
    criando classe para definir uma nova estrutura para os pokemons

--- projetos/pokedex/assets/js/poke-api.js
+++ projetos/pokedex/assets/js/poke-api.js
@@ -2,9 +2,26 @@
 
 const pokeAPI = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+    
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    return pokemon
+}
+
 pokeAPI.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then((pokemon) => convertPokeApiDetailToPokemon(pokemon))
 } // Pega os detalhes dos pokemons e transforma em JSON
 
 pokeAPI.getPokemons = (offset = 0, limit = 10) => {
