d4ccc635f21175a464385c73da23a2e16ee8c202
Kenzo Friás <kenzofrias16@gmail.com>
2025-12-22 12:04:50 -0300

    tipos de dados

--- 8-tipos-de-dados/aula01.js
+++ 8-tipos-de-dados/aula01.js
@@ -1,10 +1,22 @@
 /*
-    boolean 
-    null 
-    undefined 
-    number 
-    string 
-    symbol 
+    boolean: true e false;
+    null;
+    undefined;
+    number: 
+        +infinito, -infinito, Nan tambem são numeros;
+        para equações precisas é necessário usar meios externos;
+    string: 
+        "" - para texto único; 
+        '' - para concatenar textos;
+        `` - para textos grandes (template string);
+    symbol: torna um valor único;
 */
 
 //object
+const x = {
+    name: "kenzo",
+    age: 25
+}
+
+x['name'] = 'gabriel'
+console.log(x)
